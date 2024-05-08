3fc1791542b7e65f72a2add17c1370ab6c16c67c
Edward Stabler <es@atiyniyoj.local>
2024-05-08 08:44:28 -0700

    update

--- haskell/Mg.hs
+++ haskell/Mg.hs
@@ -10,39 +10,40 @@ data Ft = One F | Plus F deriving (Show, Eq, Ord) -- build features from base F
 type Label = ([Ft], [Ft])
 type Lex = ([String], Label)
 data PhTree = Pl Lex | Ps [PhTree] deriving (Show, Eq, Ord)
-data SO = L Lex | S (MultiSet (SO)) | O PhTree deriving (Show, Eq, Ord)
+data SO = L Lex | S (MultiSet SO) | O PhTree deriving (Show, Eq, Ord)
 type LSO = (SO, Label)
-type WS = MultiSet (LSO)
+type WS = MultiSet LSO
 
 mrg :: [SO] -> SO
 mrg sos = S (MultiSet.fromList sos)
 
 ck :: [Label] -> [Label] -- already matched features can be `forgotten'
-ck ((_:nns,nps):([],_:pps):more) = [(nns,nps), ([],pps) ] ++ (map (\label -> ([],[])) more)
+ck ((_:nns,nps):([],_:pps):more) = [(nns,nps), ([],pps) ] ++ map (const ([],[])) more
 
 t :: [LSO] -> [LSO]      -- constituents with no remembered features can be `forgotten'
 t = filter (\lso -> snd lso /= ([],[]))
 
 soSize :: SO -> Int
-soSize (S so) = foldr (\x y -> (soSize x) + y) 1 (MultiSet.toList so)
-soSize (O (Ps ts)) = foldr (\x y -> (soSize (O x)) + y) 1 ts
-soSize _ = 1
+soSize (S so) = foldr (\x y -> soSize x + y) 1 (MultiSet.toList so)
+soSize (O (Ps ts)) = foldr (\x y -> soSize (O x) + y) 1 ts
+soSize _ = 0
+
 
 maxx :: [LSO] -> LSO
-maxx lsos = foldr1 (\x y ->if ((soSize.fst) x) >= ((soSize.fst) y) then x else y) lsos
+maxx = foldr1 (\x y ->if (soSize.fst) x >= (soSize.fst) y then x else y)
 
 match:: [WS] -> ([LSO],[LSO])
 match (ws0:wss) = case List.partition ((/= []).fst.snd) (MultiSet.toList ws0) of
     ([h],others) -> let (f,plus) = (fplus.head.fst.snd) h in
-      case (List.partition ((== (One f)).head.snd.snd) others,wss) of
+      case (List.partition ((== One f).head.snd.snd) others,wss) of
         (([c],others'),[]) -> ([h,c],others')                                        -- IM
-        (([],_),ws1:wss) -> let lsos = (MultiSet.toList ws1) in let c = maxx lsos in -- EM
+        (([],_),ws1:wss) -> let lsos = MultiSet.toList ws1 in let c = maxx lsos in -- EM
           if ((/= One f).head.snd.snd) c
           then error "match complement feature clash"
           else let others' = List.filter (/= c) lsos in
             if plus && others' == others
-            then (h:c:(atb (One f) others wss), others)
-            else if wss == [] then ([h,c], others ++ others') else error ("match: too many wss")
+            then (h:c:atb (One f) others wss, others)
+            else if null wss then ([h,c], others ++ others') else error "match: too many wss"
   where
     fplus :: Ft -> (F, Bool)             -- parse the one/plus features
     fplus ft = case ft of (One f) -> (f, False); (Plus f) -> (f, True)
@@ -50,7 +51,7 @@ match (ws0:wss) = case List.partition ((/= []).fst.snd) (MultiSet.toList ws0) of
     atb :: Ft -> [LSO] -> [WS] -> [LSO]  -- collect comps with first feature f, and others
     atb _ _ [] = []
     atb ft movers (ws:wss) = case List.partition ((== ft).head.snd.snd) (MultiSet.toList ws) of
-        ([c'],others) -> if others == movers then c':(atb ft movers wss) else error "match: ATB error"
+        ([c'],others) -> if others == movers then c':atb ft movers wss else error "match: ATB error"
 
 smc :: [LSO] -> WS
 smc lsos = if smc' [] lsos then MultiSet.fromList lsos else error "smc violation"
--- haskell/MgBin.hs
+++ haskell/MgBin.hs
@@ -8,9 +8,9 @@ data Ft = C | D | N | V | A | P | Wh deriving (Show, Eq, Ord)
 type Label = ([Ft], [Ft])
 type Lex = ([String], Label)
 data PhTree = Pl Lex | Ps [PhTree] deriving (Show, Eq, Ord)
-data SO = L Lex | S (Set (SO)) | O PhTree deriving (Show, Eq, Ord)
+data SO = L Lex | S (Set SO) | O PhTree deriving (Show, Eq, Ord)
 type LSO = (SO, Label)
-type WS = Set (LSO)
+type WS = Set LSO
 
 -- merge
 mrg :: [SO] -> SO
@@ -26,13 +26,14 @@ t = filter (\lso -> snd lso /= ([],[]))
 
 -- return number of nodes in SO (here we can disregard multidominance)
 soSize :: SO -> Int
-soSize (S so) = foldr (\x y -> (soSize x) + y) 1 (Set.toList so)
-soSize (O (Ps ts)) = foldr (\x y -> (soSize (O x)) + y) 1 ts
+soSize (S so) = foldr (\x y -> soSize x + y) 1 (Set.toList so)
+soSize (O (Ps ts)) = foldr (\x y -> soSize (O x) + y) 1 ts
 soSize _ = 1
 
 -- given LSOs, return largest, i.e. the head
 maxx :: [LSO] -> LSO
-maxx lsos = foldr1 (\x y ->if ((soSize.fst) x) >= ((soSize.fst) y) then x else y) lsos
+maxx = foldr1 
+  (\x y ->if (soSize.fst) x >= (soSize.fst) y then x else y)
 
 -- given WS, return matching ([head,comp], [other LSOs])
 match:: [WS] -> ([LSO],[LSO])
