ab910e5fa3676cfb52d4b11afbe87cb958ac87d3
Edward Stabler <es@atiyniyoj.local>
2024-05-24 10:08:32 -0700

    update

--- haskell/Mg/MgH.hs
+++ haskell/Mg/MgH.hs
@@ -13,6 +13,7 @@ inc s = case s of { (('+':s'):_) -> 1 + inc [s'] ; _ -> 0 }
 -- where i = #heads needed by c-commanding selector, (h i so) = (heads, so')
 h :: Int -> SO -> ([String], SO)
 h 0 (L lex) = ([], L lex)
+h 1 (L (w,fs)) = (w, L ([],fs))
 h i (S s) =
   let ([nws],pws:pwss) = partition ((/= []).fst.head.snd) (map ell (MultiSet.toList s)) in -- partition neg WS
     case (head.fst) nws of
