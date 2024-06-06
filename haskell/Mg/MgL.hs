50980a1c5e348404e0d435d4faba765df5666bf8
Edward Stabler <es@atiyniyoj.local>
2024-06-06 12:30:03 -0700

    update

--- haskell/Mg/MgL.hs
+++ haskell/Mg/MgL.hs
@@ -10,6 +10,6 @@ ell (L lex) = ([L lex], [snd lex])
 ell (S s) = let ([nws],pws:pwss) = partition ((/= []).fst.head.snd) (map ell (MultiSet.toList s)) in -- partition neg WS
   let (so:sos,(f:_,_):labels) = nws in case ppartition (\x y -> ((==f).head.snd) y) (sos,labels) of -- partition matches
     (([so'],_), _) -> if (head.fst) pws /= so' || not (null pwss)    -- IM
-                      then error ("ell: merge-over-move violation\n\n" ++ show ((head.fst) pws) ++ "\n\n" ++ show so' ++ "\n\n")
+                      then error "ell: merge-over-move violation"
                       else d [nws]
     _ -> d (nws:pws:pwss)                                            -- EM
