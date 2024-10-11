d4c53aef4ef145bb17687c1b4f3a1e36af1dca73
Edward Stabler <es@atiyniyoj.local>
2024-10-11 10:10:20 -0700

    update

--- haskell/MgBinH.hs
+++ haskell/MgBinH.hs
@@ -1,25 +1,21 @@
 module MgBinH where
 import Data.MultiSet (MultiSet, fromList, toList) -- Multiset needed. E.g. use: ghci -package multiset
 import MgBin (SO(S,L), ell, wssNeg, wsPosMatch )
+import Data.Bifunctor (first)
 
-hfeats :: [String] -> (Int, Bool, [String])
+hfeats :: [String] -> (Int, Bool, [String]) -- returns (noOfProbes, strong?, head without feats)
 hfeats [] = (0, False, [])
-hfeats (s:ss) = let (i,s') = inc s in if length s' > 0
-                                      then if last s' == '*' then (i,True,init s':ss) else (i,False,s':ss)
-                                      else (i,False,s':ss)
-  where inc [] = (0, [])
-        inc s = if head s == '-' then let (i,s') = inc (tail s) in (1+i,s') else (0,s)
+hfeats (s:ss) = let (i,s') = deps s in if length s' > 0 && [last s'] == "*" then (i,True,init s':ss) else (i,False,s':ss)
+  where deps ('-':s') = first (1+) (deps s')
+        deps s = (0, s)
 
-heng :: SO -> SO
+heng :: SO -> SO -- h' (noOfHeadsNeededAbove,strong?,split?,headsFromAbove) = (break?,headsFromBelow,so)
 heng so = case h' 0 False False [] so of { (_, [], so') -> so' } where
-  -- h' i st sp hs so, with i = no. of heads needed above, st = strong?, sp = split?, hs = heads from above
-  --   returns (VatBot?, heads from below, so)
   h' :: Int -> Bool -> Bool -> [String] -> SO -> (Bool, [String], SO)
   h' 0 _ _ [] (L lex) = (False, [], L lex)
-  h' 1 hiStrong sp hs (L (w,fs)) = let (i',isStrong,w') = hfeats w in let cat = (head.snd.fst) fs in  -- chain ends
+  h' 1 hiStrong sp hs (L (w,fs)) = let (i',isStrong,w') = hfeats w in let cat = (head.snd.fst) fs in
     if isStrong && not hiStrong then (cat == "V", [], L(w'++hs,fs)) else (cat == "V", w'++hs, L([],fs))
-  h' i hiStrong sp hs (S s) =
-    let ([nws],pws:pwss) = wssNeg (map ell (toList s)) in case (head.fst) nws of
+  h' i hiStrong sp hs (S s) = let ([nws],pws:pwss) = wssNeg (map ell (toList s)) in case (head.fst) nws of
       L (w,fs) -> let cat = (head.snd.fst) fs in let (i',strong,w') = hfeats w in
         let i'' = i' + max 0 (i-1) in case (i,i'') of
           (0,0) -> case h' 0 False False [] ((head.fst) pws) of                  -- no chain
@@ -43,14 +39,12 @@ heng so = case h' 0 False False [] so of { (_, [], so') -> so' } where
                       if strong && not hiStrong
                       then (br, [], S (fromList (L (hs', fs) : pso : [])))
                       else (br, hs', S (fromList (L ([], fs) : pso : [])))
-      nso -> if not sp && nonMovingIntervener nws pws
+      nso -> if not sp && nonMovingPso nws pws
         then let (br,hs',nso') = h' i hiStrong True hs nso in let psos = map (head.fst) (pws:pwss) in
                (br, hs', S (fromList (nso' : psos)))
         else let (br,hs',nso') = h' i hiStrong sp hs nso in let psos = map (head.fst) (pws:pwss) in
                (br, hs', S (fromList (nso' : psos)))
-    where
-      nonMovingIntervener nws pws = let (nso:nsos,nlabel:nlabels) = nws in
-        let f = (head.fst) nlabel in case wsPosMatch f (nsos,nlabels) of
-          (([pso],[plabel]), _) -> if (length (snd plabel)) > 1 then False else True
-          (([],[]), _) -> case wsPosMatch f pws of
-            (([pso],[plabel]), _) -> if (length (snd plabel)) > 1 then False else True
+nonMovingPso nws pws = let (nso:nsos,nlabel:nlabels) = nws in
+  let f = (head.fst) nlabel in case wsPosMatch f (nsos,nlabels) of
+    (([pso],[plabel]), _) -> if (length (snd plabel)) > 1 then False else True
+    (([],[]), _) -> case wsPosMatch f pws of {(([pso],[plabel]), _) -> if (length (snd plabel)) > 1 then False else True}
--- haskell/MgBinO.hs
+++ haskell/MgBinO.hs
@@ -9,9 +9,7 @@ o_svo (S s) = let ([(nso:nsos,nlabel:nlabels)],poswss) = wssNeg (map ell (toList
                ((([pso],[plabel]), _), _) ->  formatPhTree (length (snd plabel)) nso pso -- IM
                ((([],[]), _), [ws]) -> case wsPosMatch f ws of
                  (([pso],[plabel]), _) -> formatPhTree (length (snd plabel)) nso pso -- EM
-  where
-    formatPhTree :: Int -> SO -> SO -> SO
-    formatPhTree noOfPsoPosFeats nso pso = case (noOfPsoPosFeats, nso, o_svo nso, o_svo pso) of
-      (1, L _, O pht, O pht') -> O (Ps [pht, pht']) -- first merge, nonmoving complement
-      (1, S _, O pht, O pht') -> O (Ps [pht', pht]) -- nonfirst merge, nonmoving complement
-      (_,   _, O pht, _) -> O pht                   -- moving complement
+  where formatPhTree noOfPsoPosFeats nso pso = case (noOfPsoPosFeats, nso, o_svo nso, o_svo pso) of
+            (1, L _, O pht, O pht') -> O (Ps [pht, pht']) -- first merge, nonmoving complement
+            (1, S _, O pht, O pht') -> O (Ps [pht', pht]) -- nonfirst merge, nonmoving complement
+            (_,   _, O pht, _) -> O pht                   -- moving complement
